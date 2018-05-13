f8f9cd9fab2d2e45b3bdba1232a11730f2e7dc2b
ragnarvald <ragnar@decode.is>
2018-05-13 19:26:48 +0000

    usb controller

---
+++ src/com/faradice/dali/USBDaliController.java
@@ -0,0 +1,24 @@
+package com.faradice.dali;
+
+import java.io.IOException;
+
+public class USBDaliController  extends ADaliController {
+
+	public void close() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void open() throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	void writeToDriver(char c) throws IOException {
+		// TODO Auto-generated method stub
+		
+	}
+
+}
