df848c8f920c7a3716e567f402a7225302cecd8c
ragnarvald <ragnar@decode.is>
2018-05-15 08:03:36 +0000

    start of steps to detect the device connected to USB on pi

--- src/com/faradice/dali/USBDaliController.java
+++ src/com/faradice/dali/USBDaliController.java
@@ -2,6 +2,25 @@ package com.faradice.dali;
 
 import java.io.IOException;
 
+/*
+ * Dali controller (driver) to deliver Dali commands using USB or RS232 (Serial)
+ * 
+ * Supports a connection to a standard Dali controller using USB interface on Rasperry Pi
+ * 
+ * Information regarding hardwere plugin:
+ * https://ubuntuforums.org/showthread.php?t=1716756
+ *
+ * Without USB cable connected to pi:  
+ * $ lsusb
+ * (Should not show the device) so plug in USB and run command again and device should be listed:
+ * Bus 001 Device 001: ID 1d6b:0002 Linux Foundation 2.0 root hub
+ * 
+ *  dmesg | tail
+ *  usb 1-1.5: pl2303 converter now attached to ttyUSB0
+ *  lsusb -v -d 1d6b:0002
+ * 
+ */
+
 public class USBDaliController  extends ADaliController {
 
 	public void close() throws IOException {
