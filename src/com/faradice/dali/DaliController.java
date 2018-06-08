ee6596869c08c5001d4f2c6dbc628f5c612cb3bb
ragnarvald <ragnar@decode.is>
2018-06-08 12:50:07 +0000

    Driver for new Dali protocol

--- src/com/faradice/dali/DaliController.java
+++ src/com/faradice/dali/DaliController.java
@@ -4,93 +4,56 @@ import java.io.Closeable;
 import java.io.IOException;
 
 public class DaliController extends ADaliController  implements Closeable {
+	public static String DALI_COMMAND_FILE = "NewDaliCommand";
+	
+	public static String DAL_MAX = "";
+	public static String DALI_MEDIUM = "";
+	public static String DALI_MAX = "";
+	public static String DAL_MEDIUM_LOW = "";
 
+	
 	@Override
 	public void open() throws IOException {
-		
+		// file open and close will be implementead on write
 	}
 	
 	
 	@Override
    	public void writeToDriver(char c)  throws IOException {
-		
-	}
-	
-	@Override
-	public void testDaliAddresses() throws Exception {
-		for (short i=0; i< 64; i++) {
-			System.out.println("Send Max to address: "+i);
-			daliOff("FE");
-			DaliUtil.delay(500);
-			daliMax(i+"");
-		}
-		daliOff("FE"); 
 	}
 	
 	@Override
 	public void daliMax(String addr) throws Exception {
-		if (addr.length() < 2) addr = "0"+addr;
-		toDali("010010"+addr+"FE");
+		sendCommand(DAL_MAX);
 	}
 	
 	@Override
 	public void daliMed(String addr) throws Exception {
-		if (addr.length() < 2) addr = "0"+addr;
-		toDali("010010"+addr+"E5");
+		sendCommand(DALI_MEDIUM);
 	}
 
 	@Override
 	public void daliMedLow(String addr) throws Exception {
-		if (addr.length() < 2) addr = "0"+addr;
-		toDali("010010"+addr+"C5");
+		sendCommand(DAL_MEDIUM_LOW);
 	}
 
 	@Override
 	public void daliLow(String addr) throws Exception {
-		if (addr.length() < 2) addr = "0"+addr;
-		toDali("010010"+addr+"02");
 	}
 	
 	@Override
 	public void daliOff(String addr) throws Exception {
-		if (addr.length() < 2) addr = "0"+addr;
-		toDali("010010"+addr+"00");
 	}
 	
 	@Override
 	public void daliOff(short address) throws Exception {
-		if (address < 0) {
-			address = 0xFF;
-		} else if (address > 63) {
-			throw new Exception("invalid Dali address: "+address);
-		}
-		char[] addr = DaliUtil.asciiOf(address);
-		toDali("010010"+addr[0]+addr[1]+"00");
 	}
 	
-	@Override
-	public void sendCommand(char[] command) throws Exception {
+	public void sendCommand(String msg) throws Exception {
 		System.out.println("Write cmd to dali:");
-		DaliUtil.delay(10);
-		int cc = 0;
-		for (int i = 0; i < command.length; i++) {
-			char c = command[i];
-			if (c == '\n' || c == '\r' || c == '\t' || c == ' ') {
-				continue;
-			}
-			if (c == DaliUtil.SOH) {
-				System.out.print("Z ");
-			} else if (c == DaliUtil.ETB) {
-				System.out.print("X");
-			} else {
-				cc++;
-				System.out.print(c);
-				if ((cc % 2) == 0)
-					System.out.print(" ");
-			}
-			writeToDriver(c);
-		}
-		System.out.println("\nDone");
+		DaliUtil.delay(50);
+		FaraFiles.deleteFile(DALI_COMMAND_FILE);
+		FaraFiles.appendToFile(DALI_COMMAND_FILE, msg);
 	}
 
 	@Override
@@ -98,13 +61,7 @@ public class DaliController extends ADaliController  implements Closeable {
 		if (msg == null || msg.length() < 1) {
 			return;
 		}
-		msg = msg.toUpperCase();
-		char[] chars = new char[msg.length()];
-		for (int i = 0; i < chars.length; i++) {
-			chars[i] = msg.charAt(i);
-		}
-		char[] cmd = DaliUtil.cmdFor(chars);
-		sendCommand(cmd);
+		sendCommand(msg);
 	}
 	
 	/**
@@ -115,20 +72,6 @@ public class DaliController extends ADaliController  implements Closeable {
 	 */
 	@Override
 	public void toDali(String address, String strength) throws Exception {
-		if (address.length() < 1 || strength.length() < 1) {
-			return;
-		}
-		if (address.length() > 2 || strength.length() > 2) {
-			return;
-		}
-		if (address.length() < 2) {
-			address = "0"+address;
-		}
-		if (strength.length() < 2) {
-			strength = "0"+strength;
-		}
-		String command = "010010" + address + strength; 
-		toDali(command);
 	}
 	
 	public void close() {
